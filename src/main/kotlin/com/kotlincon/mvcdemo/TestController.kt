610fb5c600538919c02c7ce1598ad1215e852289
davidecerbo <Asdrubale89>
2019-11-16 18:44:35 +0100

    delay using flux

--- src/main/kotlin/com/kotlincon/mvcdemo/TestController.kt
+++ src/main/kotlin/com/kotlincon/mvcdemo/TestController.kt
@@ -3,16 +3,18 @@ package com.kotlincon.mvcdemo
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
+import java.time.Duration.ofMillis
 
 @RestController
 @RequestMapping
 class TestController {
 
     @GetMapping("/delay")
-    fun delayed(delay: Long?): String {
+    fun delayed(delay: Long?): Mono<String> {
         val timeMillis = delay ?: 1000
-        Thread.sleep(timeMillis)
-        return "Delay of $timeMillis"
+        return Mono.just("Delay of $timeMillis")
+                .delayElement(ofMillis(timeMillis))
     }
 
 }
