d1d0ec26bee373bf4fc79e1fa980bc9264e6b399
davidecerbo <Asdrubale89>
2019-11-16 18:57:56 +0100

    add tic example

--- src/main/kotlin/com/kotlincon/mvcdemo/TestController.kt
+++ src/main/kotlin/com/kotlincon/mvcdemo/TestController.kt
@@ -3,12 +3,20 @@ package com.kotlincon.mvcdemo
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import reactor.core.publisher.toFlux
 import java.time.Duration.ofMillis
 
 @RestController
 @RequestMapping
 class TestController {
+
+    @GetMapping("/tic")
+    fun tic() : Flux<Int> = (1..Int.MAX_VALUE)
+            .toFlux()
+            .delayElements(ofMillis(1000))
+
 
     @GetMapping("/delay")
     fun delayed(delay: Long?): Mono<String> {
