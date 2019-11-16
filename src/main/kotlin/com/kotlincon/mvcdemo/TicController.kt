198875ec93309a9558a591b65185145d145d870b
davidecerbo <Asdrubale89>
2019-11-16 18:09:32 +0100

    simple controller

---
+++ src/main/kotlin/com/kotlincon/mvcdemo/TicController.kt
@@ -0,0 +1,32 @@
+package com.kotlincon.mvcdemo
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.onCompletion
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping
+class TicController {
+
+    @GetMapping("/tic")
+    suspend fun tic(): Flow<Int> = flow {
+        (1..Int.MAX_VALUE).forEach {
+            emit(it)
+            delay(1000)
+        }
+    }.onCompletion {
+        println("Finish!!!")
+    }
+
+    @GetMapping("/delay")
+    suspend fun delayed(delay: Long?): String {
+        val timeMillis = delay ?: 1000
+        delay(timeMillis)
+        return "Delay of $timeMillis"
+    }
+
+}
