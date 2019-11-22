a2697fa8223902198cbccd071bb895ab5ac74071
davidecerbo <Asdrubale89>
2019-11-22 01:30:54 +0100

    postgres config

---
+++ webapp/src/main/kotlin/com/kotlincon/mvcdemo/PostgresConfig.kt
@@ -0,0 +1,25 @@
+  
+package com.kotlincon.mvcdemo
+
+import io.r2dbc.postgresql.PostgresqlConnectionConfiguration
+import io.r2dbc.postgresql.PostgresqlConnectionFactory
+import io.r2dbc.spi.ConnectionFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class PostgresConfig {
+
+    // https://github.com/spring-projects/spring-data-r2dbc/issues/150
+
+    @Bean
+    fun connectionFactory(): ConnectionFactory = PostgresqlConnectionFactory(
+            PostgresqlConnectionConfiguration.builder()
+                    .host("localhost")
+                    .port(5432)
+                    .username("postgres")
+                    .password("password")
+                    .database("kotlinconf")
+                    .build())
+
+}
